8b32919492a3491e18c99cd987a680088e5ae92f
xia-xia-xia <2366051620@qq.com>
2022-03-09 11:28:13 +0800

    测试点积用例

--- gcn_kgat_attention.py
+++ gcn_kgat_attention.py
@@ -14,8 +14,7 @@ from numpy.random import RandomState
 
 class GraphConstructer():
     def __init__(self, max_nodes, max_seq_length, kg_fn=None, cached_graph_fn=None):
-        if kg_fn is None:
-            user_fn = 'rating_final_kg'
+        user_fn = 'rating_final_kg'
         kg_fn = 'kg.txt'
         self.max_nodes = max_nodes
         self.max_seq_length = max_seq_length
@@ -152,6 +151,7 @@ class GraphEncoder(Module):
     # forward()方法说明了每一层对数据的操作
     # def forward(self, seq,user,choose_action=False):
     def forward(self, seq, user):
+        user = (torch.LongTensor(user)).to(device)
         batch_seq_adjs = []
         batch_seq_neighbors = []
         for s in seq:
@@ -167,6 +167,7 @@ class GraphEncoder(Module):
             input_state = output_state
 
         seq_embeddings = output_state[:, :, :1, :].contiguous().squeeze()  # [N x L x d]
+
         user_emb = self.entity_user_emb(user)
         '''
         if (choose_action):
@@ -189,7 +190,10 @@ class GraphEncoder(Module):
         right = items @ self.attention_weights
         middle = user_emb * right
         output = torch.cat((user_emb, middle, right), 0).flatten()
+        print(output)
+        # print(user_emb)
         return output
+
 
 if __name__ == '__main__':
     # graph = GraphConstructer(max_nodes=20, max_seq_length=10, kg_fn=None, cached_graph_fn=None)
@@ -198,9 +202,45 @@ if __name__ == '__main__':
     embeddings = torch.FloatTensor(json.load(open(embedding_path, 'r'))['ent_embeddings'])
     entity = embeddings.shape[0]
     emb_size = embeddings.shape[1]
-    rec = GraphEncoder(entity, emb_size,user_num =6040*0.8,embeddings=embeddings, max_seq_length=10, max_node=20, hiddim=50,
+    output = GraphEncoder(entity, emb_size,user_num =6040*0.8,embeddings=embeddings, max_seq_length=10, max_node=20, hiddim=50,
                                 layers=2,cash_fn=None, fix_emb=False).to(device)
-    print(rec)
+    output_state=[[0,1,2]]
+    user_emb=[4]
+    user_like = output(output_state,user_emb)
+    entity_user_emb = torch.FloatTensor([2.2119e-02,  1.5431e-01, -1.3462e-01, -2.0840e-01,  1.0316e-01,
+         6.0314e-02, -4.6969e-02,  3.6325e-01, -1.4954e-01,  3.0862e-01,
+        -1.0560e-01, -4.9485e-02,  2.0255e-02, -1.0944e-01, -2.1185e-03,
+        -9.8946e-02, -5.9448e-02,  1.2312e-01, -2.9753e-02,  1.6686e-02,
+         4.3468e-02, -6.3488e-02,  1.0569e-01,  1.1969e-01,  4.9157e-02,
+        -9.3380e-02,  1.3321e-01, -1.7150e-01,  1.3460e-01, -5.1788e-02,
+         4.3006e-03, -1.8060e-01, -1.2939e-01, -5.7142e-03, -2.6813e-01,
+        -1.4891e-01, -1.3504e-01,  4.2394e-01, -2.1512e-02, -2.0356e-01,
+         1.6119e-01, -3.5521e-01,  3.5796e-01,  4.8034e-02,  1.7306e-01,
+         3.4239e-02,  2.1302e-02,  1.6394e-02, -2.5969e-01, -1.0587e-01,
+         3.6225e-03,  2.1393e-02, -3.5038e-02,  1.6958e-02, -4.3091e-03,
+        -9.3749e-04, -1.0550e-02,  4.8271e-02, -1.0318e-02, -9.5484e-03,
+        -5.6712e-04,  9.9557e-03, -2.3516e-03, -1.2833e-02, -8.8858e-04,
+         6.4192e-03,  1.0874e-02, -3.3720e-02, -5.6151e-03,  8.7321e-04,
+        -2.5284e-03,  4.9485e-03,  2.5253e-02,  3.0815e-02, -4.2043e-05,
+         1.9149e-02, -7.9120e-03,  2.0819e-04, -2.7587e-02, -3.5310e-03,
+         5.8832e-04,  2.6306e-02, -1.5342e-02, -1.3050e-04,  3.5746e-02,
+        -2.2094e-02,  1.8817e-02,  1.1329e-02, -2.5747e-03, -2.6005e-03,
+         5.1991e-03,  2.8432e-02,  1.1927e-01,  1.8158e-04, -4.0209e-02,
+        -3.4483e-03,  1.0086e-03, -3.6051e-03,  2.5712e-02, -3.6974e-02,
+         1.6377e-01,  1.3864e-01,  2.6026e-01, -8.1369e-02, -4.1772e-02,
+        -1.5544e-02,  2.2462e-01,  1.3289e-01,  6.8999e-02, -3.0939e-02,
+         5.3704e-03, -2.0118e-01, -1.1610e-01,  1.1726e-01,  4.1945e-01,
+        -6.4875e-02, -1.8291e-01, -2.7389e-01,  1.8872e-01,  5.2333e-02,
+        -5.8167e-02, -7.7944e-02,  2.3893e-01,  2.5747e-01, -8.5528e-04,
+        -2.0507e-01, -5.9393e-02, -1.2140e-03, -2.0496e-01,  6.8181e-02,
+         1.3680e-01, -1.4566e-01,  1.1857e-01,  2.2838e-02, -1.3332e-01,
+         1.4837e-01, -1.3935e-01,  2.6722e-02,  1.1969e-01,  1.2775e-02,
+         3.2253e-02, -8.0045e-02,  3.3319e-01,  3.7803e-03, -2.3235e-01,
+        -1.0071e-01,  4.7347e-02, -2.1991e-01, -9.9011e-02,  3.4926e-01]).to(device)
+    rec = user_like @ entity_user_emb
+    print("rec:",rec)
+
+
 
 
 
