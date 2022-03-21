f1e86a6cc70325f8a2423b9f285b4f7f65e91f44
xia-xia-xia <2366051620@qq.com>
2022-03-21 14:40:11 +0800

    renew cg_helper.py

--- cg_helper.py
+++ cg_helper.py
@@ -3,12 +3,13 @@
 def get_scores_per_user(self, user_id, data, args,
                         ignored_id=None):
     scorelist = []
+    self.batch_size = 512
     users = [user_id] * self.batch_size
     for j in range(0, self.num_items, self.batch_size):
         k = min(j + self.batch_size, self.num_items)
         feed_dict = self.prepare_feed_dict_batch(users[:(k - j)], range(j, k), data, args, ignored_user=user_id,
                                                  ignored_id=ignored_id)
-        scores = self.sess.run(self.pos, feed_dict=feed_dict)
+        scores = self.sess.run(self.pos, feed_dict=feed_dict)   #rcf.py  h253
         scores = scores.reshape(k - j)
         scorelist = np.append(scorelist, scores)
     return scorelist
@@ -72,3 +73,41 @@ def get_topk(scores: list, visited: set, k: int):
         else:
             print(f'cannot replace {repl}')
             return None, 1e9
+
+
+
+    # 服务于get_influence3函数
+    # 'verbose', type = int, default = 1, Show the results per X epochs (0, 1 ... any positive integer)'
+    def get_hessian(self, user_id, item_explained, item_removed, data, args, batch=32, verbose=1):
+        train_data = data.train_data[(data.train_data['user'] == user_id)]
+        # self.num_params = self.hidden_factor 438h    'hidden_factor', type=int, default=64'
+        res = np.zeros(shape=(self.num_params, self.num_params), dtype=np.float32)
+        for i in range(0, train_data.shape[0], batch):
+            j = min(i + batch, train_data.shape[0])
+            if verbose > 0:
+                print('hessian batch', i, j)
+            feed_dict = self.prepare_feed_dict_batch(train_data['user'][i:j], train_data['pos_item'][i:j], data, args,
+                                                     neg_items=train_data['neg_item'][i:j], user_explained=user_id,
+                                                     item_explained=item_explained,
+                                                     ignored_user=user_id, ignored_id={item_removed})
+            tmp = self.sess.run(self.hessian, feed_dict=feed_dict)
+            res += tmp
+        res = res * self.num_samples / train_data.shape[0]    #num_samples = data.train_data.shape[0]所有训练数据的行数？
+        return res + np.identity(res.shape[0]) * self.damping  # self.damping = 0.01
+
+    def get_loss_grad_individual(self, user_id, item_id, pos_item, neg_item, data, args):
+        feed_dict = self.prepare_feed_dict_batch([user_id], [pos_item], data, args,
+                                                 neg_items=[neg_item], user_explained=user_id,
+                                                 item_explained=item_id, ignored_user=user_id, ignored_id={pos_item})
+        return self.sess.run(self.loss_grads, feed_dict=feed_dict)
+
+    def get_score_influence(self, user_id, rec_id, removed_id, params_influences, data, args):
+        feed_dict = self.prepare_feed_dict(user_id, rec_id, data, args)
+        user_emb, item_emb, score = self.sess.run([self.user_embedding, self.pos_embedding, self.pos], feed_dict=feed_dict)
+
+        feed_dict = self.prepare_feed_dict(user_id, rec_id, data, args, item_ignored=removed_id)
+        feed_dict.pop(self.user)
+
+        feed_dict[self.user_embedding] = user_emb - params_influences[0, :self.hidden_factor]
+        new_score = self.sess.run(self.pos, feed_dict=feed_dict)
+        return score - new_score
